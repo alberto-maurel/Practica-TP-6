9453ab82ebbd7886ffe635a9a386d1ab039d843c
Alberto <Alberto@Alberto>
2018-03-10 12:57:33 +0100

    Path
    No me deja hacer pull, así que voy a comprobar si es por esto

---
+++ p4/src/main/java/es/ucm/fdi/model/Path.java
@@ -0,0 +1,21 @@
+package es.ucm.fdi.model;
+
+import java.util.ArrayList;
+
+import es.ucm.fdi.util.MultiTreeMap;
+
+public class Path extends Road{
+	public Path(){}
+	
+	public Path(String id, int longitud, int maxVel, Junction src, Junction dest){
+		super(id, longitud, maxVel, src, dest);
+	}
+		
+	protected int calcularVelocidadBase(int velocidadMaxima) {
+		return velocidadMaxima;
+	}
+	
+	protected int calcularFactorReduccion(int nVehiculosAveriados) {
+		return nVehiculosAveriados;
+	}
+}
